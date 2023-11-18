163f33d7658c1f3eda471fce44cf204e8e4bfcb2
Fozzie-VR <51676947+Fozzie-VR@users.noreply.github.com>
2023-11-18 08:35:56 -0800

    Updates to start refactor
    Mostly just moved some files around

---
+++ Assets/Scripts/MainMenu/MainMenuView.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KaizenApp
+{
+    public class MainMenuView : MonoBehaviour
+    {
+        
+    }
+
+}

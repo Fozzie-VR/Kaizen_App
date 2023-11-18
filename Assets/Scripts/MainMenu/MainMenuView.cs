e3d47b05b5e28833d349183b9b8da70c4d5b15cc
Fozzie-VR <51676947+Fozzie-VR@users.noreply.github.com>
2023-11-18 09:10:21 -0800

    Update MainMenuView.cs

--- Assets/Scripts/MainMenu/MainMenuView.cs
+++ Assets/Scripts/MainMenu/MainMenuView.cs
@@ -1,12 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace KaizenApp
 {
+
+    
     public class MainMenuView : MonoBehaviour
     {
-        
+        [SerializeField] UIDocument _mainMenuDocument;
+
+
+        private void Start()
+        {
+            Generate();
+        }
+
+        private void Generate()
+        {
+            var root = _mainMenuDocument.rootVisualElement;
+            var titleLabel = new Label("Welcome to Kaizen");
+            root.Add(titleLabel);
+        }
     }
 
 }

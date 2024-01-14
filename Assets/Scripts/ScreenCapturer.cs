0b29df34f121e6bb79d5e826dec52e8c8abafb49
Fozzie-VR <51676947+Fozzie-VR@users.noreply.github.com>
2024-01-14 06:53:44 -0800

    Update ScreenCapturer.cs

--- Assets/Scripts/ScreenCapturer.cs
+++ Assets/Scripts/ScreenCapturer.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -7,33 +5,29 @@ namespace KaizenApp
 {
     public static class ScreenCapturer
     {
-        //TODO: This is a hack to get the correct y offset for the screen capture.  It should be calculated based on the height of the toolbar.
-       
         public static Texture2D GetScreenCapturer(VisualElement container, float widthMultiplier, float heightMultiplier)
         {
-            int yOffset = Screen.height;
+            int yOffset = GetYOffset(container);
             Vector2 captureAreaPos = container.worldBound.position;
-            yOffset -= (int)captureAreaPos.y;
-            yOffset -= (int)captureAreaPos.y;
-            //Debug.Log("container position = " + container.style.translate.value);
-            //Debug.Log($"capture Area Pos: {captureAreaPos}");
-            //captureAreaPos.x *= widthMultiplier;
-            //captureAreaPos.y *= heightMultiplier;
             Vector2 captureAreaSize = container.worldBound.size;
-            yOffset -= (int)captureAreaSize.y;
-            Debug.Log("y offset = " + yOffset);
-            //Debug.Log($"capture Area Size: {captureAreaSize}");
-            //Debug.Log("capture area resolved width: " + container.resolvedStyle.width);
-            //Debug.Log("capture area resolved height: " + container.resolvedStyle.height);
-            //captureAreaSize.x *= widthMultiplier;
-            //captureAreaSize.y *= heightMultiplier;
             
             Texture2D screenTexture = new Texture2D((int)captureAreaSize.x, (int)captureAreaSize.y, TextureFormat.RGB24, false);
-            Rect captureRect = new Rect(captureAreaPos.x, captureAreaPos.y + yOffset, captureAreaSize.x, captureAreaSize.y);
-            //Debug.Log($"captureRect position: {captureRect.position}");
+            Rect captureRect = new Rect(captureAreaPos.x, captureAreaPos.y - yOffset, captureAreaSize.x, captureAreaSize.y);
             screenTexture.ReadPixels(captureRect, 0, 0);
             screenTexture.Apply();
             return screenTexture;
+        }
+
+        //get the difference between the distance from the top of the screen to the top of the container
+        //and the distance from the bottom of the screen to the bottom of the container
+        //to account for the difference in how textures set zero y at the bottom and UI Elements set zero y at the top
+        private static int GetYOffset(VisualElement container)
+        {
+            int screenHeight = Screen.height;
+            Vector2 containerPos = container.worldBound.position;
+            Vector2 containerSize = container.worldBound.size;
+            int yOffset = (int)containerPos.y - (screenHeight - (int)containerSize.y - (int)containerPos.y);
+            return yOffset;
         }
     }
 }
